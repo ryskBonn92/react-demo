fd6be7b12ea0703d3f87664d0dc6f92c0751f3b3
Ryosuke Miyashita <ryosuke@RyosukenoMacBook-Air.local>
2023-02-23 20:09:48 +0900

    tes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 // react-router-domのインポート
-import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 
-import HomeIcon from '@mui/icons-material/Home';
-import { makeStyles } from '@material-ui/core/styles';
+import HomeIcon from "@mui/icons-material/Home";
+import { makeStyles } from "@material-ui/core/styles";
 import {
   AppBar,
   Box,
@@ -10,21 +10,21 @@ import {
   Typography,
   Button,
   IconButton,
-} from '@mui/material';
+} from "@mui/material";
 
 // 作成済みコンポーネントのインポート
-import { Home } from './components/Home';
-import { Login } from './components/Login';
+import { Home } from "./components/Home";
+import { Login } from "./components/Login";
 
 const useStyles = makeStyles({
   root: {
-    background: 'linear-gradient(45deg, #84fab0 30%, #8fd3f4 90%)',
+    background: "linear-gradient(45deg, #84fab0 30%, #8fd3f4 90%)",
     border: 0,
     borderRadius: 3,
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    color: 'white',
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+    color: "white",
     height: 63,
-    padding: '0 30px',
+    padding: "0 30px",
   },
 });
 
@@ -32,19 +32,19 @@ function App() {
   const classes = useStyles();
   return (
     <BrowserRouter>
-      <div className='App'>
+      <div className="App">
         <Box sx={{ flexGrow: 1 }}>
-          <AppBar className={classes.root} position='static'>
+          <AppBar className={classes.root} position="static">
             <Toolbar>
-              <Link to='/'>
+              <Link to="/">
                 <IconButton>
                   <HomeIcon />
                 </IconButton>
               </Link>
-              <Typography ml={2} variant='h6' sx={{ flexGrow: 1 }}>
+              <Typography ml={2} variant="h6" sx={{ flexGrow: 1 }}>
                 test app
               </Typography>
-              <Button color='inherit' href='/login'>
+              <Button color="inherit" href="/login">
                 Login
               </Button>
             </Toolbar>
@@ -52,8 +52,8 @@ function App() {
         </Box>
         <Routes>
           {/* exact をつけると完全一致になる */}
-          <Route exact path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
+          <Route exact path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </BrowserRouter>
